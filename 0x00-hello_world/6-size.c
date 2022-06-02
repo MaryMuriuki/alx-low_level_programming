d4be70376560d4c3b8467cfb1942485b21bf6ab6
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-02 16:18:48 -0700

    Data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints different types
+ * Return: 0 exiting
+ */
+int main(void)
+{
+	printf("The size of an int: %d byte(s)\n", sizeof(int));
+	printf("The size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("The size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("The size of a float: %d byte(s)\n", sizeof(float));
+	printf("The size of a char: %d byte(s)\n", sizeof(char));
+	return (0);
+}
