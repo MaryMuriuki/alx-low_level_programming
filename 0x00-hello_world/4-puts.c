6c3b8bfbe74090e0a647bea31cc5342f2b92a32c
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-02 15:03:40 -0700

    betty yawa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-/*Printing a string*/
+/*
+ *
+ * main - it prints the "Programming is like building a puzzle
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
