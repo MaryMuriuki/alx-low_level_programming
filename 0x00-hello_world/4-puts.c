02f56ce829358ea0bde944bc84600797e63d53cb
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-02 15:56:40 -0700

    betty done did it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
- * main - Will output "Programming is like building a multilingual puzzle 
- * Returns 0 for exiting
+/**
+ * main - func prints Programming is like building a multilingual puzzle
+ * Return: 0 exits
  */
 int main(void)
 {
 	puts(" \"Programming is like building a multilingual puzzle");
 	return (0);
+}
