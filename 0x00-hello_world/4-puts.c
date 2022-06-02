57116911b1cd092aa160bbf9e6e813740c8bd9d4
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-02 15:13:02 -0700

    betty is stressful

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 /*
- *
- * main - it prints the "Programming is like building a puzzle
+ *Function main produces "Programming is like building a multilingual puzzle
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts(" \"Programming is like building a multilingual puzzle");
 	return (0);
-}
