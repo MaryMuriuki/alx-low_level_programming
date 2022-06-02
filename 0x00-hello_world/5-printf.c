2e7b3c9f60e1303fa2c3a1056772cda467df711c
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-02 15:47:29 -0700

    printf for str

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
- *main func prints with proper grammar, but the outcome is a piece of art,
- *Return 0 for exit
+/**
+ * main - func prints with proper grammar, but the outcome is a piece o *f art,
+ * Return: 0 for exit
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
