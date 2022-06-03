365267335643507aaa0894f31a6fcd8d7011b7b2
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-02 17:02:29 -0700

    print string with write  func

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
-/**
+
+i/**
  * main - prints a full sentence str
  * Return: 1 exits
  *
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
