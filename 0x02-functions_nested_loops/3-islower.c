2335d40f751980b83758d2d26b033cc460cda2a4
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-07 14:34:21 -0700

    checks for lc

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _islower - checks for lowc characters
+ * _islower - checks for lowercase characters
  * @c: character to be checked
  * Return; 1 if lc c, otherwise 0
  */
 int _islower(int c)
-{ 
+{
 	return (c >= 'a' && c <= 'z');
 }
