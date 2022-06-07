7fd6f6de27157df0c12d68b24181f845f013eb39
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-07 16:47:54 -0700

    addition of ints

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - adds two ints
+ * @i: int to be added
+ * @j: int to be added
+ * Return: result
+ */
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+
+	return (k);
+}
+
+
