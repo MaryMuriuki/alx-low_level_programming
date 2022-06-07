8ab9b5a9854aa3fd8a50f821be76145e2b895b30
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-07 13:40:07 -0700

    defining own header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "alx.h"
+/**
+ * main - func will print _putchar
+ * Return: is 0 if it exits with no error
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Nested loops anf functions
+ 0. putchar
+ 1. A function that prints the alphabet, in lowercase, followed by a new line
