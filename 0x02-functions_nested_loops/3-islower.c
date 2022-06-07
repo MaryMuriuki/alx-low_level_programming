53e064565e09e8e968ac52dff41f9fa1d7f03c45
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-07 14:31:12 -0700

    checks for lc

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checks for lowc characters
+ * @c: character to be checked
+ * Return; 1 if lc c, otherwise 0
+ */
+int _islower(int c)
+{ 
+	return (c >= 'a' && c <= 'z');
+}
