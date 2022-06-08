3f82a3d4bf44e8a3168cca1e78f8a7f7d7854fac
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-07 18:34:40 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print the frist 50 fibonacci numbers
+ * starting with 1 and 2
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	long a, b, c, next;
+
+	b = 1;
+
+	c = 2;
+
+	for (a = 1; a <= 50; ++a)
+	{
+		if (b != 20365011074)
+		{
+			printf("%ld, ", b);
+		}
+		else
+		{
+			printf("%ld\n", b);
+		}
+		next = b + c;
+		b = c;
+		c = next;
+	}
+	return (0);
+}		
