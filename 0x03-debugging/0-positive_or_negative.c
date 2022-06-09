51befe0f850205ece3025ee2b5c622c56247b594
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-09 15:14:45 -0700

    debugging task 0

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - assign a random number when executed
+ * Return: 0 if it exits without errors
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
+}
