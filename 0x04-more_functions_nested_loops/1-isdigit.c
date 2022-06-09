0e9ad77fc271b618b36e51102b236d2d482a585b
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-09 11:32:03 -0700

    digit printer

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - A func that checks for a digit0 thro' 9
+ * @c: the digit being checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
  NESTED LOOPS
  0.Write a function that checks for uppercase character
+ 1. Write a function that checks for a digit (0 through 9
