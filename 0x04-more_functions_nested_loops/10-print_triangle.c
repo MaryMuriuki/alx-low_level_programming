bec0799f2b7c24029255a7b6225e55565665b5b1
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-09 18:24:17 -0700

    tri

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,27 +8,18 @@
 
 void print_triangle(int size)
 {
+	int i, j, k;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
-	else
+	for (i = 0; i < size; i++)
 	{
-		int i, j;
-
-		for (i = 1; i <= size; i++)
-		{
-			for (j = size - i; j > 1 ; j--)
-			{
-				_putchar(' ');
-			}
-
-			for (j = 0; j < i; j++)
-			{
-				_putchar('#');
-			}
-
-			_putchar('\n');
-		}
+		for (j = size - i; j > 1; j--)
+			_putchar(' ');
+		for (k = i + j; k >= 1; k--)
+			_putchar('#');
+		_putchar('\n');
 	}
 }
