25a0a60dc6083c47da5a4ab4def1fa3e83679f90
MaryMuriuki <marymuriukinn@gmail.com>
2022-06-28 04:08:18 -0700

    memory allocation malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,12 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *rs;
-	unsigned int a = 0, b = 0, leng1 = 0, leng2 = 0;
+	unsigned int a, b, leng1, leng2;
+
+	a = 0;
+	b = 0;
+	leng1 = 0;
+	len2 = 0;
 
 	while (s1 && s1[leng1])
 		leng1++;
